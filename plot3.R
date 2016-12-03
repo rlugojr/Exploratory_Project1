5feabbc6399bb6cba0d9bbafcb2a3ff5d2dfe383
Ray Lugo, Jr <rlugojr@gmail.com>
2016-12-03 01:23:24 -0500

    Added plot3.R

---
+++ plot3.R
@@ -0,0 +1,13 @@
+png(file = "Plot2.png",
+    width = 480,
+    height = 480,
+    units = "px"
+)
+
+with(hpc_set, plot(obsDateTime, Sub_metering_1, ylab = "Energy sub metering", type = "n"))
+with(hpc_set, points(obsDateTime, Sub_metering_1, col = "black", type = "s"))
+with(hpc_set, points(obsDateTime, Sub_metering_2, col = "red", type = "s"))
+with(hpc_set, points(obsDateTime, Sub_metering_3, col = "blue", type = "s"))
+legend("topright", lty = 1, col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+dev.off()
